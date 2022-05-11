169213a8659b90eaffcba5db1f09eea2e23251e4
Josoreo <105387960+Josoreo@users.noreply.github.com>
2022-05-11 15:59:41 -0300

    Add files via upload

---
+++ Mail3.py
@@ -0,0 +1,34 @@
+import base64
+import os
+from sendgrid.helpers.mail import (
+    Mail, Attachment, FileContent, FileName,
+    FileType, Disposition, ContentId)
+from sendgrid import SendGridAPIClient
+
+message = Mail(
+    from_email='gente.usuario@gmail.com',
+    to_emails='jhpueyrredon@gmail.com',
+    subject='Prueba inmo',
+    html_content='<strong>Estimado cliente usted debe bastante dinero, vea el pdf adjunto</strong>')
+"""file_path = 'example.pdf'
+with open(file_path, 'rb') as f:
+    data = f.read()
+    f.close()
+encoded = base64.b64encode(data).decode()
+attachment = Attachment()
+attachment.file_content = FileContent(encoded)
+attachment.file_type = FileType('application/pdf')
+attachment.file_name = FileName('Informacion.pdf')
+attachment.disposition = Disposition('attachment')
+attachment.content_id = ContentId('Example Content ID')
+message.attachment = attachment
+"""
+try:
+    sendgrid_client = SendGridAPIClient(os.environ.get('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'))
+    response = sendgrid_client.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e.message)
+
